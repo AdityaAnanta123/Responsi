928518c0bda053a160afc14d8bd718f4929544a9
AdityaAnanta123 <Kadek3912@students.amikom.ac.id>
2022-04-19 12:15:35 +0700

    Responsi Pemrograman

---
+++ Karyawan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4392
+{
+    class Karyawan
+    {
+        public Karyawan(string nama, string nik, int gajibulanan)
+        {
+           Nik = nik;
+           Nama = nama;
+           GajiBulanan = gajibulanan;
+        }
+        //properties
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+    }
+
+}
